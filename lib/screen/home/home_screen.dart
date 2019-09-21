82b379e2cf7e8cfb14412ea075ec731d90f5b130
sandunsameera <sandunsameera25@gmail.com>
2019-09-21 22:12:18 +0530

    Entry point update

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,9 @@
-import 'package:Fluttery/screen/flutter_screen.dart';
+
+import 'package:Fluttery/screen/common/main_header.dart';
+import 'package:Fluttery/screen/home/flutter_screen.dart';
 import 'package:Fluttery/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,20 +14,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Edu Hack'),
-      ),
-      drawer: NavDrawer(),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Context"),
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(
-              builder:(context)=>FlutterScreen(),
-            ));
-          },
-        ),
-      ),
-    );
-  }
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[MainHeader("Edu Hack 19")];
+        },
+         body: Container(
+           decoration: BoxDecoration(
+             color: Colors.orangeAccent.withOpacity(0.2)
+           ),
+           child: Column(
+             children: <Widget>[
+               Padding(
+                 padding: EdgeInsets.all(70),
+               ),
+               FlatButton(
+                 child: Text("Join Community"),
+                 onPressed: (){
+                   Navigator.push(context,MaterialPageRoute(
+                     builder: (context)=>FlutterScreen()
+                   ));
+                 },
+               ),
+             ],
+           ),
+         ),
+    ),
+    drawer: NavDrawer(),
+    );}
 }
